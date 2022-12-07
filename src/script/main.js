59aebf69e9b22d264ec772485a3cbaebada1825d
Danillo Xavier <102252843+danxsodev@users.noreply.github.com>
2022-12-07 16:58:44 -0300

    Add files via upload

--- src/script/main.js
+++ src/script/main.js
@@ -1,82 +1,37 @@
-const itemsCart = [
-    
-    {
-        id: 0,
-        nameProduct: 'Temaki',
-        imageProduct: 'sushis.jpg',
-        priceProduct: '30,90',
-        quantityProduct: 0
-    },
+const selectItem = (elemento) => document.querySelector(elemento)
+const selectItemAll = (elemento) => document.querySelectorAll(elemento)
 
-    /*{
-        id: 1,
-        nameProduct: '',
-        imageProduct: '',
-        priceProduct: '39,90',
-        quantityProduct: 0
-    },
+const formatMonetaryReal = (valor) => {
+    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+}
 
-    {
-        id: 2,
-        nameProduct: '',
-        imageProduct: '',
-        priceProduct: '35,90',
-        quantityProduct: 0
-    },
-
-    {
-        id: 3,
-        nameProduct: '',
-        imageProduct: '',
-        priceProduct: '25,90',
-        quantityProduct: 0
-    }*/
-
-]
+const formatMonetary = (valor) => {
+    if(valor) {
+        return valor.toFixed(2)
+    }
+}
 
 function loadCard() {
-    const contentCard = document.getElementsByClassName("content-card");
-    itemsCart.map((item)=>{
-
-        var productID = document.getElementsByClassName("add-to-cart") = item.id;
-        var productName = document.getElementsByClassName("product-name") = item.nameProduct;
-        var productImage = document.getElementsByClassName("image-product") = item.imageProduct;
-        var productPrice = document.getElementsByClassName("price") = item.priceProduct;
-        /*var productQuantity = document.getElementsByClassName("").value = item.quantityProduct;*/
-
+    
+    itemsProduct.map((menuItem, item, index)=>{
+        menuItem.setAttribute('data-key', index)
+        menuItem.document.getElementsByClassName("add-to-cart").innerHMTL = item.id;
+        menuItem.document.getElementsByClassName("product-name").innerHMTL = item.nameProduct;
+        menuItem.document.getElementsByClassName("image-product").src = item.imageProduct;
+        menuItem.document.getElementsByClassName("price").innerHMTL = item.priceProduct;
+        
     })
 }
 
 const btnAddCart = document.getElementsByClassName("add-to-cart");
 
 function addItemsCart() {
-    const contentCardCart = document.getElementsByClassName("content-card-cart");
-    itemsCart.map((item)=>{
+    itemsProduct.map((item)=>{
 
-        if (item.quantityProduct > 0) {
-            
-            contentCardCart.innerHMTL = "";
-
-            contentCardCart.innerHMTL+= `
-                <div class="card-cart">
-                    <span class="img-item">
-                        <img src="`+item.imageProduct+`" alt="">
-                    </span>
-
-                    <span class="name-item">
-                        <h4>`+item.nameProduct+`</h4>
-                    </span>
-
-                    <span class="price-item">
-                        <h5 class="price">`+item.priceProduct+`</h5>
-                    </span>
-
-                    <span class="quantity-item">
-                        <p class="quantity">Quantidade: `+item.quantityProduct+`</p>
-                    </span>
-                </div>
-            `;
-        }
+        ("").src = item.img
+        ("").innerHTML = item.name
+        ("").innerHTML = item.description
+        ("").innerHTML = formatReal(item.price[2])
 
     })
 }
---
+++ src/script/menu.js
@@ -0,0 +1,92 @@
+const itemsProduct = [
+    
+    {
+        id: 0,
+        nameProduct: 'Temaki',
+        imageProduct: 'sushis.jpg',
+        priceProduct: '30,90',
+        quantityProduct: 0
+    },
+
+    {
+        id: 1,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '39,90',
+        quantityProduct: 0
+    },
+
+    {
+        id: 2,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '35,90',
+        quantityProduct: 0
+    },
+
+    {
+        id: 3,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '25,90',
+        quantityProduct: 0
+    },
+
+
+    {
+        id: 4,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '25,90',
+        quantityProduct: 0
+    },
+
+    {
+        id: 5,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '25,90',
+        quantityProduct: 0
+    },
+
+    {
+        id: 6,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '25,90',
+        quantityProduct: 0
+    },
+
+    {
+        id: 7,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '25,90',
+        quantityProduct: 0
+    },
+
+    {
+        id: 8,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '25,90',
+        quantityProduct: 0
+    },
+
+    {
+        id: 9,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '25,90',
+        quantityProduct: 0
+    },
+
+    {
+        id: 10,
+        nameProduct: '',
+        imageProduct: '',
+        priceProduct: '25,90',
+        quantityProduct: 0
+    },
+    
+]
